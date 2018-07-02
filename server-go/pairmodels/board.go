1bac10be16bac62690c60c9aa73440ef0da46e67
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-07-02 12:17:36 +0100

    rand.Shuffle moved to slices package to support Go versions < 1.10

--- server-go/pairmodels/board.go
+++ server-go/pairmodels/board.go
@@ -11,6 +11,7 @@ import (
 	"github.com/strongo/db/gaedb"
 	"google.golang.org/appengine/datastore"
 	"github.com/strongo/emoji/go"
+	"github.com/strongo/slices"
 )
 
 type PairsBoardEntity struct {
@@ -186,34 +187,10 @@ func NewCells(width, height int) string {
 		items[i] = available[randIndex]
 		items[i+pairsCount] = available[randIndex]
 	}
-	shuffle(rnd, len(items), func(i, j int) {
+	slices.Shuffle(rnd, len(items), func(i, j int) {
 		items[i], items[j] = items[j], items[i]
 	})
 
 	return strings.Join(items, ",")
 }
 
-// Shuffle pseudo-randomizes the order of elements.
-// n is the number of elements. Shuffle panics if n < 0.
-// swap swaps the elements with indexes i and j.
-func shuffle(r *rand.Rand, n int, swap func(i, j int)) {
-	if n < 0 {
-		panic("invalid argument to Shuffle")
-	}
-
-	// Fisher-Yates shuffle: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-	// Shuffle really ought not be called with n that doesn't fit in 32 bits.
-	// Not only will it take a very long time, but with 2³¹! possible permutations,
-	// there's no way that any PRNG can have a big enough internal state to
-	// generate even a minuscule percentage of the possible permutations.
-	// Nevertheless, the right API signature accepts an int n, so handle it as best we can.
-	i := n - 1
-	for ; i > 1<<31-1-1; i-- {
-		j := int(r.Int63n(int64(i + 1)))
-		swap(i, j)
-	}
-	for ; i > 0; i-- {
-		j := int(r.Int31n(int32(i + 1)))
-		swap(i, j)
-	}
-}
